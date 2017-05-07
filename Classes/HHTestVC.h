fe75e1f59abdc5f381a91648be099b38e8548b99
huanghuacai <18811108252@163.com>
2017-05-07 10:16:42 +0800

    增加信息

---
+++ Classes/HHTestVC.h
@@ -0,0 +1,13 @@
+//
+//  HHTestVC.h
+//  Test1
+//
+//  Created by 蔡万鸿 on 2017/5/7.
+//  Copyright © 2017年 黄花菜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHTestVC : UIViewController
+
+@end
---
+++ Classes/HHTestVC.m
@@ -0,0 +1,37 @@
+//
+//  HHTestVC.m
+//  Test1
+//
+//  Created by 蔡万鸿 on 2017/5/7.
+//  Copyright © 2017年 黄花菜. All rights reserved.
+//
+
+#import "HHTestVC.h"
+
+@interface HHTestVC ()
+
+@end
+
+@implementation HHTestVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
